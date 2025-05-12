caea9a3a8e483eec9bc8060ac95f6a04231f759d
AuroraHumo <carles.casallachs@gmail.com>
2025-05-12 16:37:00 +0200

    added api request and visualization data

--- src/components/Starships.tsx
+++ src/components/Starships.tsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import ApiRequests from "../data/ApiRequests";
+import { API_URL } from "../data/apiConfig";
+import { useState, useEffect } from "react";
+
+interface Starship {
+    name: string;
+    model: string;
+    manufacturer: string;
+    cost_in_credits: string;
+    length: string;
+    crew: string;
+    passengers: string;
+    cargo_capacity: string;
+    consumables: string;
+}
+
 const Starships = () => {
+    const [data, setData] = useState<Starship[]>([]);
+
+    const fetchData = async () => {
+        const answer = await fetch(API_URL.STARWARS_SHIPS);
+        const json = await answer.json();
+        setData(json.results);
+        console.log(json.results)
+    }
+            
+    useEffect(() => {
+        fetchData();
+    }
+    , []);
 
     return (
         <>
-            <h1>Starships</h1>
-            <p>This is going to import starships.</p>
-
+            <h1 className="text-2xl">Starships</h1>
+            {data.map((item: Starship, index: number) => (
+                <div key={index} className="border-2 border-gray-300 p-4 m-2">
+                    <h1> {item.name.toUpperCase()}</h1>
+                    <p> {item.model}</p>
+                </div>
+            ))}
         </>
     )
 }
---
+++ src/data/apiConfig.ts
@@ -0,0 +1,7 @@
+export const API_URL = {
+    STARWARS_SHIPS: "https://swapi.py4e.com/api/starships",    
+};
+
+export const API_KEYS = {
+    
+};
